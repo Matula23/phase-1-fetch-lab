5514ca0f0252f558b324136b263eb3263eb258f1
Maciej Matula <maciek.matula@gmail.com>
2022-06-13 17:51:15 -0500

    commit tree 070bd11b6e01670d9a773e56b508b0dc08b22e5a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then(renderBooks)
 }
 
 function renderBooks(books) {
@@ -15,3 +17,30 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+// Practice code that pulls data from api
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then((json) => {
+//     let totalPages = json.reduce((previousBook, currentBook) => currentBook["numberOfPages"] + previousBook, 0)
+//     console.log(totalPages)
+//     return totalPages
+//   })
+
+//   fetch("https://anapioficeandfire.com/api/books")
+//     .then((resp) => resp.json())
+//     .then((json) => {
+//     let characterNumber = 0
+//     let character = 1236
+//     json.find(book => {
+//       characterNumber += book["characters"].length
+//       debugger
+//       if(characterNumber >= character){
+//         let characterIndex = character - (characterNumber - book["characters"].length)
+//         console.log(book["characters"][characterIndex])
+//         debugger
+//         return book["characters"][characterIndex]
+//       }
+//     })
+//   })
